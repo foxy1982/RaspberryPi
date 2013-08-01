6d9090180160451233d8ff08c93988fde13eabb4
Dan Fox <daniel.fox@ukgateway.net>
2013-08-01 17:07:48 +0000

    Initial checkin

---
+++ flaskr/flaskr.py
@@ -0,0 +1,28 @@
+#imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+#configuration
+DATABASE = "/tmp/flaskr.db"
+DEBUG = True
+SECRET_KEY = "development_key"
+USERNAME = "admin"
+PASSWORD = "admin"
+
+#create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def connect_db():
+    return sqlite3.connect(app.config["DATABASE"])
+
+if __name__ == "__main__":
+    app.run()
---
+++ helloworld/helloworld.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_template(name=None):
+    return render_template("hello.html", name=name)
+
+@app.route("/favicon.ico")
+def favicon():
+    return redirect(url_for("static", filename="foxpi.ico"))
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("errors/error404.html")
+
+if __name__ == "__main__":
+    app.debug = False
+    app.run("0.0.0.0", 10000)
+
