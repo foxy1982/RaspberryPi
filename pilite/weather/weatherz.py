d0ddbfdc4b4086547bf9426e35fcb2a8167c5380
Ed Snow <123@123.com>
2014-12-04 21:11:57 +0000

    For loop

--- pilite/weather/weatherz.py
+++ pilite/weather/weatherz.py
@@ -6,9 +6,10 @@ sys.path.append('/home/pi/git/PiLite/Python_Examples')
 from PiLiteLib import PiLiteBoard
 
 def main():
-    sink = PiLiteBoard()
-    print ("ready")
-    sink.write("Hello")
+    for x in range (0,10):
+        sink = PiLiteBoard()
+        print ("This is the time %d" % (x)) 
+        sink.write("This is ths asd asd asd asd asd asd asd This is the time: %d" % (x))
 
 if __name__ == "__main__":
     main()
