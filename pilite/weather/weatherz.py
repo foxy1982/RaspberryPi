ec003e55acd6913f0fb78ac595debcb1334e926b
Ed Snow <123@123.com>
2014-12-04 21:21:34 +0000

    Refactoring

--- pilite/weather/weatherz.py
+++ pilite/weather/weatherz.py
@@ -5,11 +5,14 @@ sys.path.append('/home/pi/git/PiLite/Python_Examples')
 
 from PiLiteLib import PiLiteBoard
 
+def output(board, message):
+   print(message)
+   board.write(message)
+
 def main():
+    board = PiLiteBoard()
     for x in range (0,10):
-        sink = PiLiteBoard()
-        print ("This is the time %d" % (x)) 
-        sink.write("This is ths asd asd asd asd asd asd asd This is the time: %d" % (x))
+        output (board, "This is the time %d" % (x)) 
 
 if __name__ == "__main__":
     main()
